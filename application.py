58f5cfb177a7bfac20e8484a65b782783b3efc84
C-Wesley <wcooke@augusta.edu>
2024-03-17 19:36:50 -0400

    Adding tab design

--- application.py
+++ application.py
@@ -21,7 +21,7 @@ Purpose : This file shows how to add some basic functionality
 
 # Imports. Make sure PyQt5 is properly installed
 #from ui_form import Ui_MainWindow
-from ui_5 import Ui_MainWindow
+from main_form import Ui_MainWindow
 from PyQt5 import QtWidgets
 from PyQt5.QtCore import QThreadPool
 from functools import partial
@@ -39,32 +39,32 @@ def setUpDials(ui:Ui_MainWindow):
     temps = db.get_config_temperature_array() # basement to top
 
     # Set the range of the dials
-    ui.top_floor_hvac_dial_4.setRange(0, 100)
-    ui.middle_floor_hvac_dial_4.setRange(0, 100)
-    ui.bottom_floor_hvac_dial_4.setRange(0, 100)
+    ui.top_floor_hvac_dial.setRange(0, 100)
+    ui.middle_floor_hvac_dial.setRange(0, 100)
+    ui.bottom_floor_hvac_dial.setRange(0, 100)
 
     # Set the initial text and value of the dials
-    ui.top_floor_activate_on_4.setText(f"Active on: {temps[2]}")
-    ui.top_floor_hvac_dial_4.setValue(temps[2])
+    ui.top_floor_activate_on.setText(f"Active on: {temps[2]}")
+    ui.top_floor_hvac_dial.setValue(temps[2])
 
-    ui.middle_floor_activate_on_4.setText(f"Active on: {temps[1]}")
-    ui.middle_floor_hvac_dial_4.setValue(temps[1])
+    ui.middle_floor_activate_on.setText(f"Active on: {temps[1]}")
+    ui.middle_floor_hvac_dial.setValue(temps[1])
 
-    ui.bottom_floor_activate_on_4.setText(f"Active on: {temps[0]}")
-    ui.bottom_floor_hvac_dial_4.setValue(temps[0])
+    ui.bottom_floor_activate_on.setText(f"Active on: {temps[0]}")
+    ui.bottom_floor_hvac_dial.setValue(temps[0])
 
     # Connect the dial value to the text label
-    ui.top_floor_hvac_dial_4.valueChanged.connect(
-    lambda: ui.top_floor_activate_on_4.setText(
-    f"Active on: {ui.top_floor_hvac_dial_4.value()}"))
+    ui.top_floor_hvac_dial.valueChanged.connect(
+    lambda: ui.top_floor_activate_on.setText(
+    f"Active on: {ui.top_floor_hvac_dial.value()}"))
 
-    ui.middle_floor_hvac_dial_4.valueChanged.connect(
-    lambda: ui.middle_floor_activate_on_4.setText(
-    f"Active on: {ui.middle_floor_hvac_dial_4.value()}"))
+    ui.middle_floor_hvac_dial.valueChanged.connect(
+    lambda: ui.middle_floor_activate_on.setText(
+    f"Active on: {ui.middle_floor_hvac_dial.value()}"))
 
-    ui.bottom_floor_hvac_dial_4.valueChanged.connect(
-    lambda: ui.bottom_floor_activate_on_4.setText(
-    f"Active on: {ui.bottom_floor_hvac_dial_4.value()}"))
+    ui.bottom_floor_hvac_dial.valueChanged.connect(
+    lambda: ui.bottom_floor_activate_on.setText(
+    f"Active on: {ui.bottom_floor_hvac_dial.value()}"))
 
     # Debuggin print statements
     # ui.top_floor_hvac_dial.valueChanged.connect(lambda: print(f"Active on: {ui.top_floor_hvac_dial.value()}"))
@@ -77,8 +77,8 @@ def setUpAlarm(ui:Ui_MainWindow):
     functions. 
     """
 
-    ui.arm_alarm_button_4.clicked.connect(lambda: print("Alarm Armed!"))
-    ui.disarm_alarm_button_4.clicked.connect(lambda: print("Alarm Disarmed!"))
+    ui.arm_alarm_button.clicked.connect(lambda: print("Alarm Armed!"))
+    ui.disarm_alarm_button.clicked.connect(lambda: print("Alarm Disarmed!"))
 
 def setUpDoor(ui:Ui_MainWindow, aDoor:Door):
     """ 
@@ -86,21 +86,21 @@ def setUpDoor(ui:Ui_MainWindow, aDoor:Door):
     functions. 
     """
 
-    ui.lock_door_button_4.clicked.connect(lambda: aDoor._close_lock())
-    ui.unlock_door_button_4.clicked.connect(lambda: aDoor._open_lock())
+    ui.lock_door_button.clicked.connect(lambda: aDoor._close_lock())
+    ui.unlock_door_button.clicked.connect(lambda: aDoor._open_lock())
 
-    ui.lock_door_button_4.clicked.connect(lambda: aDoor._log_to_database(0, "close"))
-    ui.unlock_door_button_4.clicked.connect(lambda: aDoor._log_to_database(0, "open"))
+    ui.lock_door_button.clicked.connect(lambda: aDoor._log_to_database(0, "close"))
+    ui.unlock_door_button.clicked.connect(lambda: aDoor._log_to_database(0, "open"))
 
-    ui.lock_door_button_4.clicked.connect(lambda: set_up_logs(ui))
-    ui.unlock_door_button_4.clicked.connect(lambda: set_up_logs(ui))
+    ui.lock_door_button.clicked.connect(lambda: set_up_logs(ui))
+    ui.unlock_door_button.clicked.connect(lambda: set_up_logs(ui))
 
 def set_up_logs(ui:Ui_MainWindow):
     """
     This method will query the database for new logs and update the list view. 
     """
     print("Setting up logs..")
-    ui.listWidget.clear()
+    ui.logs_list.clear()
     db = Database()
     logs = db.get_log_string_array()
     print("---")
@@ -111,11 +111,7 @@ def set_up_logs(ui:Ui_MainWindow):
     for log in logs:
         item = QtWidgets.QListWidgetItem()
         item.setText(log)
-        ui.listWidget.addItem(item)
-
-def setup_menus(ui:Ui_MainWindow):
-    """ Fill this out later wesley """
-    ui.logs_btn.clicked.connect(ui.click_change_page)
+        ui.logs_list.addItem(item)
 
 def initiate_background_thread():
     ''' TODO: figure out why threading stuff makes this function blocking. 
@@ -131,6 +127,7 @@ if __name__ == "__main__":
     This file makes a QApplication() object and connects the Ui_MainWindow() object 
     to th
     """
+
     import sys
     db = Database()
     db.initialize_db()
@@ -138,7 +135,6 @@ if __name__ == "__main__":
     MainWindow = QtWidgets.QMainWindow()
     ui = Ui_MainWindow()
     ui.setupUi(MainWindow)
-    ui.stackedWidget.setCurrentWidget(ui.page_3)
     
     # Create a door that assoicates user id 3 with it...
     ourDoor = Door()
@@ -155,8 +151,6 @@ if __name__ == "__main__":
     setUpAlarm(ui)
     setUpDoor(ui, ourDoor)
     set_up_logs(ui)
-    setup_menus(ui)
 
     MainWindow.show()
-    sys.exit(app.exec_())
-    GPIO.cleanup()  
+    sys.exit(app.exec_()) 
