1a36a7ef575d9878183c21a2322b5352ec3edc1e
C-Wesley <wcooke@augusta.edu>
2024-04-29 04:18:50 -0400

    Fixing stuffs

--- RFID.py
+++ RFID.py
@@ -13,7 +13,7 @@ from datetime import datetime, timedelta
 # from Crypto.PublicKey import RSA
 # from Crypto.Signature import pkcs1_15
 # from Crypto.Hash import SHA256
-from pirc522 import RFID
+from PIRC522.pirc522 import RFID
 
 from database import Database
 
